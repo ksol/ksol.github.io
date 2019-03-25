0ec77fb21221046789c57e49cd51dc289404ad98
Kevin Soltysiak <k@ksol.fr>
2019-03-25 12:37:18 +0100

    Remove unavailability

--- js/contact.js
+++ js/contact.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://formspree.io/contact@ksol.fr",
+                url: "https://formspree.io/contact@ksol.fr",
                 type: "POST",
                 data: {
                     name: name,
